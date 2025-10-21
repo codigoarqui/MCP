eee2c65a68c4763a2cb3b6683582584662d14353
codigoarqui <codigoarqui.examples@gmail.com>
2025-10-21 15:26:50 -0500

    deploy

--- main.py
+++ main.py
@@ -69,4 +69,5 @@ def dividir(a: float, b: float) -> float:
 
 
 if __name__ == "__main__":
-    app.run(transport="http", host="127.0.0.1", port=8000)
+    app.run()
+    # app.run(transport="http", host="127.0.0.1", port=8000)
